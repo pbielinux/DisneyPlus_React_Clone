89a87cc47df061aaf6c8d9ea796cbb1f659ae143
Pedro henrique Bielik <pbielik@c1r5s1.42lisboa.com>
2021-04-29 15:09:34 +0100

    All functions added

--- src/components/Detail/index.js
+++ src/components/Detail/index.js
@@ -3,26 +3,72 @@ import {
 	Background,
 	ImageTitle,
 	ContentMeta,
-	Controls
+	Controls,
+	Player,
+	Trailer,
+	AddList,
+	GroupWatch,
+	SubTitle,
+	Description
 } from './DetailElements';
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import db from '../../firebase';
 
 const Detail = () => {
+	const { id } = useParams();
+	const [ detailData, setDetailData ] = useState({});
+
+	useEffect(() => {
+		db.collection('movies')
+			.doc(id)
+			.get()
+			.then((doc) => {
+				if(doc.exists) {
+					setDetailData(doc.data());
+				} else {
+					console.log('no such document in firebase 🔥');
+				}
+			})
+			.catch((error) => {
+				console.log('Error getting documents:', error);
+			});
+	}, [ id ]);
+
 	return (
 		<Container>
 			<Background>
-				<img
-					src='https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/49B92C046117E89BC9243A68EE277A3B30D551D4599F23C10BF0B8C1E90AEFB6/scale?width=1440&aspectRatio=1.78&format=jpeg'
-					alt=''
-				/>
+				<img src={detailData.backgroundImg} alt={detailData.title} />
 			</Background>
 			<ImageTitle>
-				<img
-					src='https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/5C647DF3FFBFA343CFEA84AC715148F25F9E86F398B408010CC403E7654FB908/scale?width=1440&aspectRatio=1.78'
-					alt=''
-				/>
+				<img src={detailData.titleImg} alt={detailData.title} />
 			</ImageTitle>
 			<ContentMeta>
-				<Controls>controls</Controls>
+				<Controls>
+					<Player>
+						<img src='/images/play-icon-black.png' alt='' />
+						<span>Play</span>
+					</Player>
+					<Trailer>
+						<img src='/images/play-icon-white.png' alt='' />
+						<span>Trailer</span>
+					</Trailer>
+					<AddList>
+						<span />
+						<span />
+					</AddList>
+					<GroupWatch>
+						<div>
+							<img src='/images/group-icon.png' alt='' />
+						</div>
+					</GroupWatch>
+				</Controls>
+				<SubTitle>
+					{detailData.subTitle}
+				</SubTitle>
+				<Description>
+					{detailData.description}
+				</Description>
 			</ContentMeta>
 		</Container>
 	);
